5a1ea0e4bf211b3a16e275851b4108229f9ef1c9
yunweneric <mbongeyanoella@gmail.com>
2020-04-22 16:59:23 +0200

    Initialisation of work. HTML and Css, no Javascript

---
+++ script.js.js
@@ -0,0 +1,19 @@
+
+// function display2(){
+//     var main = document.querySelector("#main");
+//     var sentence1 = "This is the first setence!!!";
+//     var sentence2 = "What up guys?? this is the second setence"
+//     if (main.innerHTML == sentence1){
+//         main.innerHTML = sentence2;
+//         main.style.padding = "0px"
+//     }
+//     else{
+//         main.innerHTML = sentence1;
+//         main.style.padding = "20px"
+//     }
+    
+// }
+
+
+
+//Calculator
